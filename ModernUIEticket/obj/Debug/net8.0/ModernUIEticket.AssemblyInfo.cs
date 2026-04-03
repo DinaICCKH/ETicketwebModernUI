d4d345e68ce55e71de30129d6b67d3bbf823be28
Vong Dina <dina.vong@icckh.com>
2026-04-03 11:20:50 +0700

    Update from my old computer

--- ModernUIEticket/obj/Debug/net8.0/ModernUIEticket.AssemblyInfo.cs
+++ ModernUIEticket/obj/Debug/net8.0/ModernUIEticket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModernUIEticket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87a656c201ed9272861312eea05e7dde5f71099b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6b8c7b187e8407387d74d385446f43519592b2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModernUIEticket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModernUIEticket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
